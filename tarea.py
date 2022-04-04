98b345252114ec1ebc834d71159cfb0d2c34fd4b
Araos827 <vlaraos@uc.cl>
2022-04-03 22:39:53 -0400

    funcion1

--- tarea.py
+++ tarea.py
@@ -4,6 +4,12 @@ with open("farmers-protest-tweets-2021-03-5.json", "r", encoding='utf-8', errors
     tweets = [json.loads(line) for line in file]
     file.close()
 
+def f1():
+    lista1 = (sorted(tweets, key = lambda i: i["retweetCount"],reverse=True))
+    lista1 = lista1[0:10]
+    for element in lista1:
+        print(element["content"] + "\n")
+
 if __name__ == '__main__':
     #f1()
     #f2()
